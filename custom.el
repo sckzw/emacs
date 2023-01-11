5864c9c7262d813ba378c692328b2949a2623ced
shuichi <shuichi.kozawa@gmail.com>
2023-01-12 01:07:42 +0900

    Update for WSL

--- custom.el
+++ custom.el
@@ -7,9 +7,12 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes '(wombat))
+ '(package-selected-packages
+   '(yasnippet flycheck visual-regexp-steroids pcre2el orderless eglot use-package kind-icon corfu consult vertico company multiple-cursors))
  '(recentf-save-file "~/.emacs.d/.recentf")
- '(w32-use-w32-font-dialog t))
+ '(w32-use-w32-font-dialog t)
+ '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -12,21 +12,23 @@
 ;;
 ;(set-default-coding-systems 'utf-8-unix)
 
-;;; 日本語入力
-(setq default-input-method "W32-IME")
-;; 日本語入力の文字コード
-(set-keyboard-coding-system 'sjis)
-;; モードラインのインジケータ設定
-(setq-default w32-ime-mode-line-state-indicator "(Aa)")
-(setq w32-ime-mode-line-state-indicator-list '("(Aa)" "(あ)" "(Aa)"))
-;; ON/OFF時のカーソル色設定
-(set-cursor-color "brown")
-(add-hook 'input-method-activate-hook   (lambda() (set-cursor-color "brown")))
-(add-hook 'input-method-inactivate-hook (lambda() (set-cursor-color "wheat")))
-;; バッファ切り替え時にIME状態を引き継ぐ
-(setq w32-ime-buffer-switch-p nil)
-;; 初期化
-(w32-ime-initialize)
+(when (eq system-type 'windows-nt)
+  ;;; 日本語入力
+  (setq default-input-method "W32-IME")
+  ;; 日本語入力の文字コード
+  (set-keyboard-coding-system 'sjis)
+  ;; モードラインのインジケータ設定
+  (setq-default w32-ime-mode-line-state-indicator "(Aa)")
+  (setq w32-ime-mode-line-state-indicator-list '("(Aa)" "(あ)" "(Aa)"))
+  ;; ON/OFF時のカーソル色設定
+  (set-cursor-color "brown")
+  (add-hook 'input-method-activate-hook   (lambda() (set-cursor-color "brown")))
+  (add-hook 'input-method-inactivate-hook (lambda() (set-cursor-color "wheat")))
+  ;; バッファ切り替え時にIME状態を引き継ぐ
+  (setq w32-ime-buffer-switch-p nil)
+  ;; 初期化
+  (w32-ime-initialize)
+  )
 
 ;;; Lisp
 ;; ロードパス設定
@@ -35,17 +37,17 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
-;;; Package
 (require 'package)
-;; リポジトリ追加
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; 初期化
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(setq package-archive-priorities '(("gnu" . 15) ("nongnu" . 10) ("melpa-stable" . 5)))
 (package-initialize)
 
 ;;; バックアップ
 ;; 対象ファイルとバックアップ先ディレクトリ設定
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backup")))
+
+;;; recentf
+(recentf-mode 1)
 
 ;;; Desktop
 ;; (desktop-save-mode 1)
@@ -64,26 +66,6 @@
 (setq frame-title-format "%b")
 ;; ツールバーを非表示
 (tool-bar-mode -1)
-;; アクティブ時と非アクティブ時の透明度を設定
-(add-to-list 'default-frame-alist '(alpha . (90 80)))
-
-;;; フォント
-(set-frame-font "Consolas 11")
-;; Unicode
-(set-fontset-font nil 'mule-unicode-0100-24ff (font-spec :family "Arial Unicode MS"))
-(set-fontset-font nil 'mule-unicode-2500-33ff (font-spec :family "Arial Unicode MS"))
-(set-fontset-font nil 'mule-unicode-e000-ffff (font-spec :family "Arial Unicode MS"))
-;; 日本語
-(set-fontset-font nil 'japanese-jisx0213.2004-1 (font-spec :family "MS Gothic"))
-(set-fontset-font nil 'japanese-jisx0213-2      (font-spec :family "MS Gothic"))
-(set-fontset-font nil 'katakana-jisx0201        (font-spec :family "MS Gothic"))
-;; ラテン
-(set-fontset-font nil 'latin-iso8859-1    (font-spec :family "Consolas"))
-(set-fontset-font nil 'latin-iso8859-2    (font-spec :family "Consolas"))
-(set-fontset-font nil 'latin-iso8859-3    (font-spec :family "Consolas"))
-(set-fontset-font nil 'latin-iso8859-4    (font-spec :family "Consolas"))
-(set-fontset-font nil 'cyrillic-iso8859-5 (font-spec :family "Consolas"))
-(set-fontset-font nil 'greek-iso8859-7    (font-spec :family "Consolas"))
 
 ;;; モードライン
 ;; 行番号をモードラインに表示
@@ -120,11 +102,11 @@
 
 ;;; 検索
 ;; 検索で大文字/小文字を区別する(M-cで切り替え可能)
-(setq case-fold-search nil)
+(setq-default case-fold-search nil)
 
 ;;; キーバインド
 ;; バッファ一覧を変更
-(global-set-key "\C-x\C-b" 'bs-show)
+;(global-set-key "\C-x\C-b" 'bs-show)
 ;; バッファ移動をShift + カーソルで行う
 (windmove-default-keybindings)
 ;; バッファ移動をループさせる
@@ -143,10 +125,6 @@
     (message "init time: %.3f sec"
              (float-time (time-subtract after-init-time before-init-time)))))
 
-;;; align
-;; タブ位置で揃えない
-(setq align-to-tab-stop nil)
-
 ;;; cua-mode
 ;; 矩形編集を有効にする
 (cua-mode t)
@@ -156,213 +134,143 @@
 ;; カーソル行を強調する
 (global-hl-line-mode)
 
-;;; verilog-mode
-;; (eval-after-load "verilog-mode"
-;;   '(progn
-;;      ;; 基本設定
-;;      (setq verilog-indent-level              4
-;;            verilog-indent-level-module       4
-;;            verilog-indent-level-declaration  4
-;;            verilog-indent-declaration-macros nil
-;;            verilog-indent-lists              t
-;;            verilog-indent-level-behavioral   4
-;;            verilog-indent-level-directive    4
-;;            verilog-case-indent               4
-;;            verilog-cexp-indent               4
-;;            verilog-auto-newline              nil
-;;            verilog-auto-indent-on-newline    t
-;;            verilog-tab-always-indent         nil
-;;            verilog-tab-to-comment            t
-;;            verilog-auto-endcomments          t
-;;            verilog-indent-begin-after-if     t
-;;            verilog-align-ifelse              t
-;;            verilog-auto-lineup               nil
-;;            verilog-minimum-comment-distance  40
-;;            verilog-highlight-p1800-keywords  t
-;;            verilog-linter                    "verilator_bin --lint-only -Wall")
-;;      ;; 数値リテラルのフェイス設定
-;;      (font-lock-add-keywords
-;;       'verilog-mode
-;;       '(("\\(\\<[1-9][0-9_]*\\)?'h[0-9_a-fxz]+\\>" . font-lock-string-face)   ; 16進数
-;;         ("\\(\\<[1-9][0-9_]*\\)?'d[0-9_xz]+\\>"    . font-lock-string-face)   ; 10進数
-;;         ("\\(\\<[1-9][0-9_]*\\)?'o[0-7_xz]+\\>"    . font-lock-string-face)   ; 8進数
-;;         ("\\(\\<[1-9][0-9_]*\\)?'b[01_xz]+\\>"     . font-lock-string-face)   ; 2進数
-;;         ("\\<[0-9][0-9_]*\\>"                      . font-lock-string-face))) ; 基数なし
-;;      ))
-
 ;;; vc
 ;; 無効にする
 (setq vc-handled-backends nil)
 
-;;; whitespace
-;; 空白を強調する
-(global-whitespace-mode)
-;; 強調する空白を設定
-(setq whitespace-style
-      '(face
-        trailing
-        lines-tail
-        empty
-        space-after-tab
-        space-before-tab
-        space-mark
-        tab-mark
-        newline-mark))
-;; 空白表示のマッピング
-(setq whitespace-display-mappings
-      '(
-        (space-mark ?\u3000 [?﹏])
-        (newline-mark ?\n [?↵ ?\n])
-        (tab-mark ?\t [?\t ?⇥] [?\\ ?\t])))
+(use-package consult
+  :bind
+  (("C-x b" . consult-buffer)
+   ("M-g g" . consult-goto-line)
+   ("M-y"   . consult-yank-pop))
+  :config
+  (consult-customize
+   consult-theme consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   :preview-key '(:debounce 0.5 any)))
 
-;;; package.el管理パッケージの設定
+(use-package corfu
+  :init
+  (global-corfu-mode)
+  :custom
+  (corfu-auto t)
+  (corfu-cycle t))
 
-;;; auto-complete
-;; (require 'auto-complete-config)
-;; ;; 辞書ディレクトリ指定
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/lisp/auto-complete/dict")
-;; ;; 履歴ファイル指定
-;; (setq ac-comphist-file "~/.emacs.d/lisp/auto-complete/history.txt")
-;; ;; デフォルト設定
-;; (ac-config-default)
-;; ;; 1文字で補完を自動開始する
-;; (setq ac-auto-start 1)
-;; ;; コメント内や文字列内でも補完を有効にする
-;; (setq ac-disable-faces nil)
+(use-package eglot
+  :hook
+  (verilog-mode . eglot-ensure)
+  :config
+  (add-to-list 'eglot-stay-out-of 'flymake)
+  (add-to-list 'eglot-server-programs '(verilog-mode . ("veridian")))
+  :custom
+  (completion-category-defaults nil))
 
-(setq auto-mode-alist
-      (append
-       '(
-         ("\\.c$"    . c-mode)
-         ("\\.h$"    . c-mode)
-         ("\\.java$" . java-mode)
-         ("\\.c\\+\\+$" . c++-mode)
-         ("\\.cpp$"  . c++-mode)
-         ("\\.cc$"   . c++-mode)
-         ("\\.hh$"   . c++-mode)
-         )
-       auto-mode-alist))
+(use-package flycheck
+  :hook
+  (verilog-mode)
+  :config
+  (flycheck-define-checker verilog-verilator
+    "A Verilog syntax checker using the Verilator Verilog HDL simulator."
+    :command
+    ("verilator_bin" "--lint-only" "-Wall" "--quiet-exit"
+     (eval verilog-library-flags) source)
+    :error-patterns
+    ((warning line-start "%Warning"
+              (? "-" (id (+ (any "0-9A-Z_")))) ": "
+              (? (file-name) ":" line ":" (? column ":") " ")
+              (message) line-end)
+     (error line-start "%Error"
+            (? "-" (id (+ (any "0-9A-Z_")))) ": "
+            (? (file-name) ":" line ":" (? column ":") " ")
+            (message) line-end))
+    :modes
+    (verilog-mode)))
 
-;;; cc-mode
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            ;; インデント設定
-            (setq tab-width 4)
-            (setq c-basic-offset 4)
-            (c-set-offset 'arglist-intro '+)
-            (c-set-offset 'arglist-close 0)
-            (c-set-offset 'brace-list-open 0)
-            (c-set-offset 'cpp-macro-cont '+)
-            (c-set-offset 'substatement-open 0)
-            ;; ドキュメントコメントのスタイル設定
-            (setq c-doc-comment-style
-                  '((c-mode   . javadoc)
-                    (c++-mode . javadoc)))
-            ))
+(use-package kind-icon
+  :after corfu
+  :custom
+  (kind-icon-default-face 'corfu-default)
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
-;;; clang-format
-(require 'clang-format)
-(global-set-key [C-tab] 'clang-format-region)
+(use-package multiple-cursors
+  :bind
+  (("C-*" . mc/edit-lines)
+   ("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)))
 
-;;; company
-;; 全てのバッファで有効
-(add-hook 'after-init-hook 'global-company-mode)
-(with-eval-after-load 'company
-  ;; Backendsにcompany-ironyを追加
-  (add-to-list 'company-backends 'company-irony)
-  ;; 補完開始までの時間
-  (setq company-idle-delay 0.2)
-  ;; 補完開始に必要な最低文字数
-  (setq company-minimum-prefix-length 2)
-  ;; 補完候補選択のラップアラウンドを有効
-  (setq company-selection-wrap-around t)
-  ;; 補完表示設定
-  (custom-set-faces
-   '(company-tooltip                  ((t (:inherit region                  ))))
-   '(company-tooltip-selection        ((t (:background "brown"              ))))
-   '(company-tooltip-search           ((t (:inherit isearch                 ))))
-   '(company-tooltip-mouse            ((t (:inherit highlight               ))))
-   '(company-tooltip-common           ((t (:inherit region     :underline t ))))
-   '(company-tooltip-common-selection ((t (:background "brown" :underline t ))))
-   '(company-tooltip-annotation       ((t (:inherit region                  ))))
-   '(company-scrollbar-fg             ((t (:inherit default                 ))))
-   '(company-scrollbar-bg             ((t (:inherit region                  ))))
-   '(company-preview                  ((t (:inherit region                  ))))
-   '(company-preview-common           ((t (:background "brown"              ))))
-   '(company-preview-search           ((t (:inherit isearch                 ))))))
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
-;;; flycheck
-;; モード別設定
-(add-hook 'c-mode-hook 'flycheck-mode)
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'emacs-lisp-mode 'flycheck-mode)
-;; (add-hook 'verilog-mode 'flycheck-mode)
-;; Verilogチェッカのverilatorの引数にverilog-library-filesを追加
-;;(eval-after-load "flycheck"
-;  '(progn
-;     (flycheck-define-checker verilog-verilator
-;       "A Verilog syntax checker using the Verilator Verilog HDL simulator."
-;       :command ("verilator_bin" "--lint-only" "-Wall" (eval verilog-library-files) source)
-;       :error-patterns ((warning line-start "%Warning-" (zero-or-more not-newline) ": " (file-name) ":" line ": " (message) line-end)
-;                        (error line-start "%Error: " (file-name) ":" line ": " (message) line-end))
-;       :modes (verilog-mode))
-;     ))
-(with-eval-after-load 'flycheck
-  (require 'flycheck-irony)
-  (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
-  (flycheck-add-next-checker 'irony '(warning . c/c++-cppcheck)))
+(use-package verilog-mode
+  :custom
+  (verilog-indent-level                4   )
+  (verilog-indent-level-module         4   )
+  (verilog-indent-level-declaration    4   )
+  (verilog-indent-lists                nil )
+  (verilog-indent-level-behavioral     4   )
+  (verilog-indent-level-directive      4   )
+  (verilog-cexp-indent                 4   )
+  (verilog-case-indent                 4   )
+  (verilog-auto-newline                nil )
+  (verilog-auto-indent-on-newline      nil )
+  (verilog-indent-begin-after-if       nil )
+  (verilog-align-ifelse                nil )
+  (verilog-minimum-comment-distance    40  )
+  (verilog-highlight-grouping-keywords t   )
+  (verilog-highlight-modules           t   )
+  (verilog-highlight-includes          t   )
+  (verilog-auto-lineup                 nil )
+  (verilog-linter "verilator_bin --lint-only -Wall")
+  :config
+  ;; 数値リテラルのフェイス設定
+  (font-lock-add-keywords
+   'verilog-mode
+   '(("\\(\\<[1-9][0-9_]*\\)?'h[0-9_a-fxz]+\\>" . font-lock-string-face)   ; 16進数
+     ("\\(\\<[1-9][0-9_]*\\)?'d[0-9_xz]+\\>"    . font-lock-string-face)   ; 10進数
+     ("\\(\\<[1-9][0-9_]*\\)?'o[0-7_xz]+\\>"    . font-lock-string-face)   ; 8進数
+     ("\\(\\<[1-9][0-9_]*\\)?'b[01_xz]+\\>"     . font-lock-string-face)   ; 2進数
+     ("\\<[0-9][0-9_]*\\>"                      . font-lock-string-face))) ; 基数なし
+  )
 
-;;; helm
-(require 'helm-config)
-(helm-mode t)
-(global-set-key (kbd "C-:") 'helm-mini)
+(use-package vertico
+  :init
+  (vertico-mode))
 
-;;; irony
-;; モード設定
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'c++-mode-hook 'irony-mode)
-;; Compilation Databaseから自動で設定を抽出
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-;; company-mode設定
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; キーバインド設定
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(use-package visual-regexp-steroids
+  :bind
+  (("C-M-s" . vr/isearch-forward)
+   ("C-M-r" . vr/isearch-backward)
+   ("C-M-%" . vr/query-replace)
+   ("C-c m" . vr/mc-mark))
+  :custom
+  (vr/engine 'pcre2el))
 
-;;; multiple-cursors
-;; キーバインド設定
-(global-set-key (kbd "C-*") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(use-package whitespace
+  :init
+  (global-whitespace-mode)
+  :custom
+  (whitespace-style
+   '(face
+     trailing
+     lines-tail
+     empty
+     space-after-tab
+     space-before-tab
+     space-mark
+     tab-mark
+     newline-mark))
+  (whitespace-display-mappings
+   '((space-mark ?\u3000 [?﹏])
+     (newline-mark ?\n [?↵ ?\n])
+     (tab-mark ?\t [?\t ?⇥] [?\\ ?\t]))))
 
-;;; popwin
-(require 'popwin)
-(popwin-mode 1)
-;; ウィンドウを下部に表示する
-(setq popwin:popup-window-position 'bottom)
-;; コンパイル結果ウィンドウを指定
-(push '(compilation-mode :stick t) popwin:special-display-config)
-;; Helmウィンドウを指定
-(push '("helm" :regexp t) popwin:special-display-config)
-
-;;; visual-regexp-steroids
-(require 'visual-regexp-steroids)
-;; 正規表現をPCREからElispに変換
-(setq vr/engine 'pcre2el)
-;; キーバインド設定: 標準の検索・置換
-(global-set-key (kbd "C-M-s") 'vr/isearch-forward)
-(global-set-key (kbd "C-M-r") 'vr/isearch-backward)
-(global-set-key (kbd "C-M-%") 'vr/query-replace)
-;; キーバインド設定: multiple-cursorsマーク
-(global-set-key (kbd "C-c m") 'vr/mc-mark)
-
-;;; yasnippet
-;; モード設定
-(add-hook 'c-mode-hook #'yas-minor-mode)
-(add-hook 'c++-mode-hook #'yas-minor-mode)
+(use-package yasnippet
+  :hook
+  (verilog-mode . yas-minor-mode))
 
 ;;; init.el ends here
